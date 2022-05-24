6ae75f559f00a649eccb7cddb6d881e1040047f4
Annitdy <104436851+Annitdy@users.noreply.github.com>
2022-05-24 20:35:33 +0800

    Create ex5-1.cpp

---
+++ ex5-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+	
+	for(int x=1;x<=9;x=x+1)
+	{
+	for(int a=1;a<=9;a=a+1)
+	{
+	cout<<a<<"*"<<x<<"="<<a*x<<"\t";
+
+	}
+		cout<<endl;
+
+	
+}
+	return 0;
+	} 
+	
