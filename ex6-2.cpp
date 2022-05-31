ddf393d369553615397f0467030a52e732f4ccd1
Annitdy <104436851+Annitdy@users.noreply.github.com>
2022-05-31 21:07:08 +0800

    Create ex6-2.cpp

---
+++ ex6-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+	
+    int x,y,a;
+    cin>>x>>y;
+    cin>>a;
+    if(a!=x+y)
+    cout<<"error";
+    else
+    cout<<"right";
+	
+
+	return 0;
+	} 
